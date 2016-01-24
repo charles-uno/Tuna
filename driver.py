8b51c5b61fec08578f49296331fbb4663460ef1c
Charles <charles.a.mceachern@gmail.com>
2016-01-24 01:02:27 -0600

    switched over to compressional

--- driver.py
+++ driver.py
@@ -27,8 +27,8 @@ runDirName = 'tuna'
 
 # Tuna includes default values, which it uses for any parameter not specified. 
 parameters = { 
-              'jdrive':[1e-4], 
-#              'bdrive':[10], 
+#              'jdrive':[1e-4], 
+              'bdrive':[10], 
               'tmax':[300],
               'azm':[1, 4, 16, 64],
               'model':[1, 2, 3, 4],
